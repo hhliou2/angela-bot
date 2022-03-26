4bd1b2d1707a1ff19da703afd3eeee32b341d115
yunghas <43155295+yunghas@users.noreply.github.com>
2022-03-25 17:43:31 -0700

    new message function added

--- src/messaging.py
+++ src/messaging.py
@@ -13,7 +13,7 @@ class Messaging(commands.Cog):
     # Gives a user a Gayness rating of 0-100. Note that Baraboomba is defaulted to the 90-100 range.
     @commands.command()
     async def gay(self, ctx):
-        if (ctx.author.id == 352964311423123458):
+        if (ctx.author.id == 352964311423123458 or ctx.author.id == 194902708535164934):
             gayness = random.randint(90,100)
         else:
             gayness = random.randint(0,100)
@@ -21,6 +21,12 @@ class Messaging(commands.Cog):
         # Write message as an embed
         embed = discord.Embed(description = f'**{ctx.author.name}** is ==> **{gayness}%** gay', color=11342935)
         await ctx.channel.send(embed=embed)
+
+
+    # Returns the phrase "Shut the fuck up Baraboomba" along with a silenced seagull
+    @commands.command()
+    async def baraboomba(self, ctx):
+        await ctx.channel.send("Shut the fuck up baraboomba https://imgur.com/a/S0l1n2P")
 
 # Give main bot all commands in this file
 def setup(bot):
