ede9da31d2473ca0f56e5d41a1f27e482618636e
stapler8 <tait.brendan@gmail.com>
2022-03-26 00:57:31 -0400

    added roles.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,2 @@
 import src.messaging
+import src.roles
---
+++ src/roles.py
@@ -0,0 +1,20 @@
+# import dependencies
+
+import discord
+from discord.ext import commands
+
+
+class Roles(commands.Cog):
+    # Upon instantiation, set local variable bot to bot entity
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await member.send("Welcome, fren :D")
+        role = discord.utils.get(member.guild.roles, name="new member")
+        await member.add_roles(member, role)
+
+
+def setup(bot):
+    bot.add_Cog(Roles(bot))
