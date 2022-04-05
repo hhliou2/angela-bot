455d6e5e1c71d84c9dc3d8ec111c253ff16732ab
stapler8 <tait.brendan@gmail.com>
2022-04-04 20:50:15 -0400

    Added NSFW role selection Added nsfw.json Gave each reaction type its own function

--- src/roles.py
+++ src/roles.py
@@ -6,7 +6,7 @@ import json
 
 msg = None
 reactions = ['\N{THUMBS UP SIGN}', '\N{THUMBS DOWN SIGN}']
-clist = []
+ndict = {}
 cdict = {}
 userreact = None
 # client = discord.client
@@ -14,6 +14,9 @@ userreact = None
 
 with open("./config/colours.json") as f:
     colours = json.load(f)
+
+with open("./config/nsfw.json") as f:
+    nsfw = json.load(f)
 
 
 class Roles(commands.Cog):
@@ -23,33 +26,11 @@ class Roles(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        # await member.send("Welcome, fren :D")
         role = discord.utils.get(member.guild.roles, name="new member")
         await member.add_roles(role)
 
-    # @commands.command(aliases=['color', 'colors', 'colours'])
-    # async def colour(self, ctx):
-    #     print('printing colour message')
-    #     # channel = ctx.channel
-    #     await ctx.message.delete()
-    #     global msg
-    #     global clist
-    #     global userreact
-    #     userreact = ctx.author
-    #     print(userreact)
-    #     clist.clear()
-    #     msg = await ctx.channel.send('React to me to choose your colour!')
-    #     for colour in colours:
-    #         await msg.add_reaction(colour[0])       # react with all the colours in our dict
-    #         clist.append(colours[colour])
-    #     await asyncio.sleep(10)
-    #     await msg.delete()
-    #     try:
-    #         await msg.delete()
-    #     except discord.errors.NotFound:
-    #         print('already deleted')
-
     @commands.command(aliases=['color', 'colors', 'colours'])
+    @commands.cooldown(1, 10, commands.BucketType.user)
     async def colour(self, ctx):
         print('printing colour message')
         await ctx.message.delete()
@@ -61,37 +42,38 @@ class Roles(commands.Cog):
         cdict[ctx.author.id] = await ctx.channel.send('React to me to choose your colour!')
         for colour in colours:
             await cdict[ctx.author.id].add_reaction(colour[0])
-        # print(cdict)
-        await asyncio.sleep(10)
+        await asyncio.sleep(60)
         await cdict[ctx.author.id].delete()
 
-    # @commands.Cog.listener()
-    # async def on_reaction_add(self, reaction, user):
-    #     global msg
-    #     role = None
-    #     if user.bot or user != userreact:
-    #         return
-    #     if reaction.emoji in colours:
-    #         role = discord.utils.get(user.guild.roles, name=colours[reaction.emoji])
-    #     # channel = reaction.channel
-    #     if reaction.message == msg and role:
-    #         for userrole in user.roles:
-    #             if userrole.name in clist:
-    #                 await user.remove_roles(userrole)
-    #                 print('remove_role success', userrole)
-    #         await user.add_roles(role)
-    #         print('add_role success', role)
-    #         await asyncio.sleep(1)
-    #         if msg is not None:
-    #             await msg.delete()
+    @commands.command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def nsfw(self, ctx):
+        print('printing nsfw message')
+        await ctx.message.delete()
+        global msg
+        global ndict
+        while ctx.author.id in ndict.keys():
+            print(f'user {ctx.author} already has request opened')
+            ndict.pop(ctx.author.id)
+        ndict[ctx.author.id] = await ctx.channel.send('React to me to choose your NSFW role!')
+        for i in nsfw:
+            await ndict[ctx.author.id].add_reaction(i[0])
+        await asyncio.sleep(60)
+        await cdict[ctx.author.id].delete()
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        role = None
-        if user.bot or user.id not in cdict.keys():
+        if user.bot or user.id not in cdict.keys() and user.id not in ndict.keys():
             return
+        if reaction.emoji in colours and reaction.message == cdict[user.id]:
+            await self.colour_react(reaction, user)
+        if reaction.emoji in nsfw and reaction.message == ndict[user.id]:
+            await self.nsfw_react(reaction, user)
+
+    async def colour_react(self, reaction, user):
+        role = None
         if reaction.emoji in colours:
-            role = discord.utils.get(user.guild.roles, name=colours[reaction.emoji])
+            role = discord.utils.get(user.guild.roles, name=colours[reaction.emoji])    # get role name from reaction
         if reaction.message == cdict[user.id]:
             for userrole in user.roles:
                 if userrole.name in colours.values():
@@ -104,20 +86,28 @@ class Roles(commands.Cog):
                 await cdict[user.id].delete()
                 cdict.pop(user.id)
 
-
-    @commands.Cog.listener()
-    async def on_reaction_remove(self, reaction, user):
-        global msg
+    async def nsfw_react(self, reaction, user):
         role = None
-        if reaction.emoji in colours:
-            role = discord.utils.get(user.guild.roles, name=colours[reaction.emoji])
-        if user.bot:
-            return
-        if reaction.message == msg and role:
-            await user.remove_roles(role)
-            print('remove_role success', role)
+        if reaction.emoji in nsfw:
+            role = discord.utils.get(user.guild.roles, name=nsfw[reaction.emoji])
+        if reaction.message == ndict[user.id]:
+            for userrole in user.roles:
+                if userrole.name in nsfw.values():
+                    await user.remove_roles(userrole)
+                    print(f'remove_role success {userrole}')
+            await user.add_roles(role)
+            print(f'add_role success {role}')
+            await asyncio.sleep(1)
+            if user.id in ndict.keys():
+                await ndict[user.id].delete()
+                ndict.pop(user.id)
 
     @colour.error                         # stop the bot from throwing an error when it tries to delete a message twice
+    async def info_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            print('already deleted :)')
+
+    @nsfw.error                           # ditto
     async def info_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
             print('already deleted :)')
