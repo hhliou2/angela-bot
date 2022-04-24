79983456e20a28eb4b940a8084274ae553af40d4
stapler8 <tait.brendan@gmail.com>
2022-04-24 06:27:09 -0400

    Changed: !gay command now accepts an optional user argument

--- src/messaging.py
+++ src/messaging.py
@@ -4,31 +4,35 @@ import random
 import discord
 from discord.ext import commands
 
+
 class Messaging(commands.Cog):
-    
+
     # Upon instantiation, set local variable bot to bot entity
     def __init__(self, bot):
         self.bot = bot
 
     # Gives a user a Gayness rating of 0-100. Note that Baraboomba is defaulted to the 90-100 range.
     @commands.command()
-    async def gay(self, ctx):
-        if (ctx.author.id == 352964311423123458 or ctx.author.id == 194902708535164934):
-            gayness = random.randint(90,100)
-        elif (ctx.author.id == 216683868244148225):
-            gayness = random.randint(100, 200)
+    async def gay(self, ctx, *, user=None):
+        if user:
+            gayness = random.randint(0, 100)
+            embed = discord.Embed(description=f'**{user}** is ==> **{gayness}%** gay', color=11342935)
+            await ctx.channel.send(embed=embed)
         else:
-            gayness = random.randint(0,100)
-
-        # Write message as an embed
-        embed = discord.Embed(description = f'**{ctx.author.name}** is ==> **{gayness}%** gay', color=11342935)
-        await ctx.channel.send(embed=embed)
-
+            if ctx.author.id == 352964311423123458 or ctx.author.id == 194902708535164934:
+                gayness = random.randint(90, 100)
+            elif ctx.author.id == 216683868244148225:
+                gayness = random.randint(100, 200)
+            else:
+                gayness = random.randint(0, 100)
+            embed = discord.Embed(description=f'**{ctx.author.name}** is ==> **{gayness}%** gay', color=11342935)
+            await ctx.channel.send(embed=embed)
 
     # Returns the phrase "Shut the fuck up Baraboomba" along with a silenced seagull
     @commands.command()
     async def baraboomba(self, ctx):
         await ctx.channel.send("Shut the fuck up baraboomba https://imgur.com/a/S0l1n2P")
+
 
 # Give main bot all commands in this file
 def setup(bot):
